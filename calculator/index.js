2235666a67b9ce33d5d00c3e6dc7024e8ae51e4f
Jooyoung Kim <141282000+teddy-1991@users.noreply.github.com>
2024-11-22 18:36:30 -0700

    added an add, equal features

--- calculator/index.js
+++ calculator/index.js
@@ -2,14 +2,61 @@ const buttonsValue = document.querySelectorAll(".number-button");
 const displayedNumber = document.getElementById("display");
 const clearNumber = document.getElementById("clear");
 
-
 buttonsValue.forEach(button => {
     button.addEventListener("click", (event) => {
-        displayedNumber.value += event.target.textContent;
-    })
-})
+        if (isTypingSecondNumber) {
+            displayedNumber.value = event.target.textContent;
+            isTypingSecondNumber = false;
+        } else {
+            displayedNumber.value += event.target.textContent;
+        }
+        
+    });
+});
 
 clearNumber.addEventListener("click", () => {
     displayedNumber.value = null;
-})
+    firstNumber = null;
+    operator = null;
+    isTypingSecondNumber = false;
+});
 
+// Variables to store data
+let firstNumber = null;
+let operator = null;
+let isTypingSecondNumber = false;
+
+
+// 
+const addButton = document.getElementById("add");
+const equalButton = document.getElementById("equal");
+
+
+addButton.addEventListener("click", () => {
+    if (firstNumber === null) {
+        firstNumber = parseFloat(displayedNumber.value);
+    } else if (operator === "+") {
+        const secondNumber = parseFloat(displayedNumber.value);
+        const result = firstNumber + secondNumber;
+        firstNumber = result;
+        displayedNumber.value = result;
+    }
+
+    operator = "+";
+    isTypingSecondNumber = true;
+});
+
+
+equalButton.addEventListener("click", () => {
+    if (firstNumber !== null && operator === "+") {
+        const secondNumber = parseFloat(displayedNumber.value);
+
+        if (!isNaN(secondNumber)) {
+            const result = firstNumber + secondNumber;
+            displayedNumber.value = result;
+            firstNumber = result;
+            operator = null;
+            isTypingSecondNumber = false;
+        }
+    }
+});
